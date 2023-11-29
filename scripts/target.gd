7471f1d53481802e36ad4a0884a0b71d184de3f0
PureAsbestos <16106576+PureAsbestos@users.noreply.github.com>
2023-11-29 15:07:00 -0600

    Get shooting to work with raycast

--- scripts/target.gd
+++ scripts/target.gd
@@ -1,8 +1,4 @@
 extends Sprite3D
 
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-func _input(event: InputEvent):
-	if  event is InputEventMouseButton && event.pressed && event.button_index == MOUSE_BUTTON_LEFT:
-		get_parent().remove_child(self)
+func got_hit():
+	queue_free()

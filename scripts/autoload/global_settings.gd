05cf825ae2fc4a8367cdf14e4701af13d15ab084
svg190000 <97975415+svg190000@users.noreply.github.com>
2023-11-29 19:00:17 -0600

    add timer

--- scripts/autoload/global_settings.gd
+++ scripts/autoload/global_settings.gd
@@ -3,10 +3,10 @@ extends Node
 var fullscreen = false
 var vSync = false
 var sensVal = 1
+var timer: Timer = Timer.new()
 
 func instance_node(node, location, parent):
 	var node_instance = node.instantiate()
 	parent.add_child(node_instance)
 	node_instance.global_position = location
 	return node_instance
-
--- scripts/levelScripts/gridShot.gd
+++ scripts/levelScripts/gridShot.gd
@@ -4,12 +4,54 @@ var levelNum = 1
 var target = preload("res://scenes/target.tscn")
 var maxTargets = false # max targets on screen at a time is 5
 var counter = 0
+@onready var animate  = $"../AnimationPlayer"
 
 const MIN_X : float = 10
 const MAX_X : float = 15
 const MIN_Y : float = 3
 const MAX_Y : float = 5
 const Z : float = -12.5
+
+# copy this into all level code
+var startTime: int
+var sec: int
+var minute = 0
+var timeout = false
+
+func _ready():
+	await animate.animation_finished
+	startTime = 4
+	sec = startTime
+	$timer.start(1)
+
+func startGame():
+	startTime = 60
+	sec = startTime
+	$timer.start(1)
+	$targetSpawnTimer.start(1)
+
+func timer_timout():
+	game_time()
+
+func game_time():
+	if sec > 1:
+		sec -= 1
+		if startTime == 4:
+			$startCountDown.text = str(sec)
+		elif startTime == 60:
+			$gameCountDown.text = str(minute) + ":" + str(sec)
+	elif sec == 1:
+		sec -= 1
+		$startCountDown.text = "GO!"
+		if startTime == 60:
+			$gameCountDown.text = str(minute) + ":" + str(sec)
+			$timer.stop()
+	if sec == 0:
+		$startCountDown.hide()
+		if startTime == 4:
+			startGame()
+		else:
+			timeout = true
 
 func randomize_position() -> Vector3:
 	randomize()
@@ -23,14 +65,10 @@ func _on_target_spawn_timer_timeout():
 		var location = randomize_position()
 		var temp = GlobalSettings.instance_node(target, location, self)
 		temp.name = ("target" + str(counter))
-		$targetSpawnTimer.start(1)
 		counter += 1
 		if counter == 5:
 			maxTargets = true
 	else:
 		counter -= 1
 		maxTargets = false
-
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
+		print(maxTargets)
